67e629aa90e09cf93b201b4b695fd9b3e89e5e53
Saurabh Bandkar <saurabh.bandkar.16@mountblue.tech>
2021-06-24 00:24:09 +0530

    Create single database connection for multiple API queries

--- routes/routeData.js
+++ routes/routeData.js
@@ -1,28 +1,29 @@
 const express = require('express');
 const router = express.Router();
 const mysql = require('mysql');
+const connection = require('./connectDB')
 
-router.get('/showdatabases', (request, response) => {
-    const connection = mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        password: 'Mysql#021',
-        database: 'ipl'
-    });
-    connection.connect((err) => {
+
+router.get('/viewusers', (request, response) => {
+    connection.query('SHOW TABLES', (err, result) => {
         if (err) {
             throw err;
         } else {
-            connection.query('SHOW TABLES', (err, result) => {
-                if (err) {
-                    throw err;
-                } else {
-                    response.status(200).json(result);
-                    //console.log(JSON.stringify(result));
-                };
-            });
+            response.status(200).json(result);
+            //console.log(JSON.stringify(result));
         };
     });
 });
 
+
+router.get('/viewalluserstasks', (request, response) => {
+    connection.query('SELECT COUNT(*) FROM ipl.matches', (err, result1) => {
+        if (err) {
+            throw err;
+        } else {
+            response.status(200).json(result1);
+            //console.log(JSON.stringify(result));
+        };
+    });
+});
 module.exports = router;
